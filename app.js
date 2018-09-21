506d72dcb6ef8663d73f42480bbd68407b536742
jeffer <jeffe.bernal@gmail.com>
2018-09-21 15:51:11 -0500

    await for info

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-const express = require("express");
+const express = require('express');
+const app = express();
 const mongoose = require("mongoose");
 
-const app = express();
-
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1', { useNewUrlParser: true });
+mongoose.connection.on("error",function(e){console.error(e);});
+app.use(express.urlencoded());
 
-const VisitorSchema = new mongoose.Schema({
-  name: { type: String },
-  date: { type: Date, default: Date.now }
+// definimos el schema
+var schema = mongoose.Schema({date:Date,name:String});
+// definimos el modelo
+var Visitor = mongoose.model("Visitor", schema);
+
+app.get('/', (req, res) => {
+  let name = (req.query.name == undefined || req.query.name == '') ? 'Anónimo' : req.query.name
+  var visitor = new Visitor({ date: Date.now(), name: name })
+  visitor.save(function(err, visitor) {
+    if (err) return console.error(err);
+    console.log(visitor)
+    res.send('<h1>El visitante fue almacenado con éxito</h1>');
+  });
 });
-const Visitor = mongoose.model("Visitor", VisitorSchema);
 
-app.get("/", async (req, res) => {
-  const visitor = new Visitor({ name: req.params.name || "Anónimo" });
-  await visitor.save()
-
-  res.send("<h1>El visitante fue almacenado con éxito.</h1>")
-});
-
-app.listen(3000, () => console.log("Listening on port 3000 ..."));
+app.listen(3000, () => console.log('listening 3000'));
